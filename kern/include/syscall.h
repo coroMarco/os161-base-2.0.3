9e6213caa791997f54ce39e4735b99b1c7ed0f28
coroMarco <coronellimarco@gmail.com>
2025-12-08 12:58:54 +0100

    feat: add function (getpid & getppid) in syscall.h

--- kern/include/syscall.h
+++ kern/include/syscall.h
@@ -90,7 +90,8 @@ int sys___time(userptr_t user_seconds, userptr_t user_nanoseconds);
 	int sys_dup2(int oldfd,int newfd,int *retval);
 	void sys__exit(int status);
 	int sys_waitpid(pid_t pid, userptr_t statusp, int options);
-	pid_t sys_getpid(void);
+	int sys_getpid(pid_t *retpid);
+	int sys_getppid(int *retpid);
 	int sys_fork(struct trapframe *ctf, pid_t *retval);
 	int sys_execv(const char *progname,char *argv[]);
 #endif
